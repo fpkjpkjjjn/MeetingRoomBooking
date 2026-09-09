2e975570e84b3ebec3a474ee2cc2948fbb1cd197
fpkjpkjjjn <fpkjpkjjjn@gmail.com>
2026-09-09 22:36:20 +0200

    Clear

--- MeetingRoomBooking/Services/IBookingService.cs
+++
@@ -1,9 +0,0 @@
-﻿using MeetingRoomBooking.DTOs;
-using MeetingRoomBooking.Models;
-
-namespace MeetingRoomBooking.Services {
-    public interface IBookingService {
-        Task<(bool Success, string? ErrorMessage, Booking? Booking)> CreateBookingAsync(CreateBookingDto dto);
-        Task<(bool Success, string? ErrorMessage, Booking? Booking)> UpdateBookingAsync(int id, UpdateBookingDto dto);
-    }
-}
