6de360f7741705ec9404440c0e09a59d27a59577
fpkjpkjjjn <fpkjpkjjjn@gmail.com>
2026-09-11 00:18:07 +0200

    Docker add

--- MeetingRoomBooking/Program.cs
+++ MeetingRoomBooking/Program.cs
@@ -11,6 +11,12 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope()) {
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.Migrate();
+}
+
 app.UseDefaultFiles();
 app.UseStaticFiles(); 
 
